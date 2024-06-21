2c476bc0a51dd1ead6ec0ad2b57917ff9c7a3726
Rodrigo Limpias <rodrigo.limpias@incrowdsports.com>
2024-06-21 12:14:44 +0200

    Initial Commit

---
+++ Ebury/EburyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EburyApp.swift
+//  Ebury
+//
+//  Created by Rodrigo Limpias Cossio on 21/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EburyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
